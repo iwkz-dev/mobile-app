b3de4601ba5f35e12e9a3c5e80f5330734cb9fc7
fathonitr <ododmengapa@gmail.com>
2023-11-08 12:48:37 +0100

    #9 Testing API Req

--- src/screens/homeScreen.js
+++ src/screens/homeScreen.js
@@ -1,13 +1,47 @@
-import * as React from 'react';
-import { Text, View } from 'react-native';
+import React, {useState, useEffect} from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import { url } from '../utils/config';
 
 function HomeScreen() {
-    return (
-      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center', 
-      backgroundColor: "lightgrey"}}>
-        <Text>Home!</Text>
-      </View>
+    const [data, setData] = useState([]);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        fetch(url)
+            .then((resp) => resp.json())
+            .then((json) => setData(json))
+            .catch((error) => console.error(error))
+            .finally(() => setLoading(false));
+    }, []);
+
+    return(
+        <View style={styles.container}>
+        {loading ? (
+            <Text>Loading...</Text>
+        ) : (
+            data.map((post) => {
+                return (
+                    <View>
+                        <Text style={styles.title}>{post.title}</Text>
+                        <Text>{post.body}</Text>
+                    </View>
+                );
+            })
+        )}
+    </View>
     );
-  }
+}
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: "center",
+        backgroundColor: "grey",
+        padding: 8,
+    },
+    title: {
+        fontSize: 30,
+        fontWeight: "bold",
+    },
+});
 
 export default HomeScreen;
