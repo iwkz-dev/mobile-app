d85233b677eee555a4e7cb2458df9593538bdf1e
fathonitr <58855134+fathonitr@users.noreply.github.com>
2023-11-08 09:39:57 +0100

    Setup/navigation (#8)
    * #7 Setup/React-Navigation
    
    * #7 Testing Navigator
    
    * #7 added Screen for each feature

--- App.js
+++ App.js
@@ -1,9 +1,20 @@
-import React from 'react';
-import { Text } from 'react-native';
+import * as React from 'react';
+import { Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import HomeScreen from './src/screens/homeScreen';
+import CalenderScreen from './src/screens/calendarScreen';
+import ZakatDonasiScreen from './src/screens/zakatDonasiScreen';
+const Tab = createBottomTabNavigator();
 
-const App = () => {
-    return(
-        <Text>Hello</Text>
-    );
-};
-export default App;
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Tab.Navigator>
+        <Tab.Screen name="Home" component={HomeScreen} />
+        <Tab.Screen name="Calendar" component={CalenderScreen} />
+        <Tab.Screen name="ZakatDonasi" component={ZakatDonasiScreen}/>
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
+}
---
+++ src/screens/calendarScreen.js
@@ -0,0 +1,13 @@
+import * as React from 'react';
+import { Text, View } from 'react-native';
+
+function CalenderScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center', 
+    backgroundColor: "lightgrey" }}>
+      <Text>Calendar!</Text>
+    </View>
+  );
+}
+
+export default CalenderScreen;
