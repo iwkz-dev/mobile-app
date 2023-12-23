25e7324612e2ca721c2823b1c0579f30537f32e8
fathonitr <ododmengapa@gmail.com>
2023-12-23 17:12:03 +0100

    #17 Working native Calendar, next is integeration with google

--- src/screens/calendarScreen.js
+++ src/screens/calendarScreen.js
@@ -1,11 +1,13 @@
 import * as React from 'react';
 import { Text, View } from 'react-native';
-
+import {Calendar, LocaleConfig} from 'react-native-calendars'
 function CalenderScreen() {
   return (
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center', 
     backgroundColor: "lightgrey" }}>
-      <Text>Calendar!</Text>
+      <Calendar
+      
+      />
     </View>
   );
 }
