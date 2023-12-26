bd2026434c104f38205e178829c56d1fad0cd726
fathonitr <ododmengapa@gmail.com>
2023-12-27 00:24:01 +0100

    #18 Integration with google calendar working

--- src/screens/calendarScreen.js
+++ src/screens/calendarScreen.js
@@ -1,11 +1,46 @@
-import * as React from 'react';
+import React, {useEffect, useState} from 'react';
 import { Text, View } from 'react-native';
-import {Calendar, LocaleConfig} from 'react-native-calendars'
+import { Calendar, LocaleConfig } from 'react-native-calendars'
+import { calendarURL, apiKey } from '../utils/config';
 function CalenderScreen() {
+  const [events, setEvents] = useState({});
+  useEffect(() => {
+    fetch(calendarURL)
+      .then((resp) => resp.json())
+      .then((json) => {
+        // Process the response and extract events for the current month
+        const filteredEvents = json.items.filter((event) => {
+          const eventDate = new Date(event.start.dateTime);
+          return (
+            //Only store events from current month and year
+            eventDate.getMonth() === new Date().getMonth() &&
+            eventDate.getFullYear() === new Date().getFullYear()
+          );
+        });
+        // Convert events to a format suitable for react-native-calendars
+        const formattedEvents = {};
+        filteredEvents.forEach((event) => {
+          const date = event.start.dateTime.split("T");
+          console.log(date[0])
+          formattedEvents[date[0]] = {selected: true, marked: true, dotColor: 'blue' };
+        });
+        setEvents(formattedEvents);
+      })
+      .catch((error) => console.error(error));
+  }, []);
+
+
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center', 
-    backgroundColor: "lightgrey" }}>
+    <View style={{
+      flex: 1, justifyContent: 'center', alignItems: 'center',
+      backgroundColor: "lightgrey"
+    }}>
       <Calendar
+        markedDates={events}
+        // Other calendar props can be added here
+        onDayPress={day => {
+          console.log('selected day', day.dateString);
+        }}
       
       />
     </View>
